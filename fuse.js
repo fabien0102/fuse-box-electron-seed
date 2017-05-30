a9383ae6e6d09f0b7a21427f33cb6a8d2f7f2ba3
fabien0102 <fabien0102@planee.fr>
2017-05-30 13:24:19 +0200

    Import packaging process
    Ref: https://github.com/chentsulin/electron-react-boilerplate

--- fuse.js
+++ fuse.js
@@ -1,33 +1,47 @@
 const {
-    FuseBox,
-    SassPlugin,
-    CSSPlugin,
-    Sparky
+  FuseBox,
+  SassPlugin,
+  CSSPlugin,
+  EnvPlugin,
+  Sparky
 } = require("fuse-box");
 
-const {spawn} = require("child_process");
-
+const { spawn } = require("child_process");
+const isDev = process.env.NODE_ENV === "development";
 
 Sparky.task("copy-html", () => {
-    return Sparky.src("src/index.html").dest("dist/$name");
+  return Sparky.src("src/index.html").dest("dist/$name");
 });
 
 Sparky.task("default", ["copy-html"], () => {
-    const fuse = FuseBox.init({
-        homeDir: "src",
-        output: "dist/$name.js"
-    });
-    // development server for hot reload
-    fuse.dev({port: 4445, httpServer: false});
+  const fuse = FuseBox.init({
+    homeDir: "src",
+    output: "dist/$name.js"
+  });
 
-    fuse.bundle("app")
-        .target("electron")
-        .plugin(SassPlugin(), CSSPlugin({group: "bundle.css"}))
-        .watch()
-        .hmr()
-        .instructions(" > [index.ts]"); // it's import to isolate like this []
+  if (isDev) {
+    // development server for hot reload
+    fuse.dev({ port: 4445, httpServer: false });
+
+    fuse
+      .bundle("app")
+      .target("electron")
+      .plugin(SassPlugin(), CSSPlugin({ group: "bundle.css" }))
+      .watch()
+      .hmr()
+      .instructions(" > [index.ts]"); // it's import to isolate like this []
     return fuse.run().then(() => {
-        // launch the app
-        spawn('node', [`${ __dirname }/node_modules/electron/cli.js`,  __dirname ]);
+      // launch the app
+      spawn("node", [`${__dirname}/node_modules/electron/cli.js`, __dirname]);
     });
+  } else {
+    // bundle app for production (with dependencies)
+    fuse
+      .bundle("index")
+      .target("electron")
+      .plugin(EnvPlugin({ NODE_ENV: process.env.NODE_ENV }))
+      .instructions("> index.ts");
+
+    return fuse.run();
+  }
 });
